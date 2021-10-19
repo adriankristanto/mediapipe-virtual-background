091dd4d65c20c86dee9a1fe9325819c79d307476
Adrian Kristanto <49110670+adriankristanto@users.noreply.github.com>
2021-10-20 00:41:53 +1100

    added initial working virtual background

--- app/src/App.js
+++ app/src/App.js
@@ -48,7 +48,48 @@ const App = () => {
     }, []);
 
     const onResults = (results) => {
-        console.log(results);
+        // the following two lines of code fix the resolution of the canvas
+        canvasRef.current.width = webcamRef.current.video.clientWidth;
+        canvasRef.current.height = webcamRef.current.video.clientHeight;
+
+        const canvasCtx = canvasRef.current.getContext("2d");
+        // https://stackoverflow.com/questions/50681592/getusermedia-mirror-image-instead-of-flip
+        canvasCtx.setTransform(-1, 0, 0, 1, canvasRef.current.width, 0);
+        canvasCtx.save();
+        canvasCtx.clearRect(
+            0,
+            0,
+            canvasRef.current.width,
+            canvasRef.current.height
+        );
+        canvasCtx.drawImage(
+            results.segmentationMask,
+            0,
+            0,
+            canvasRef.current.width,
+            canvasRef.current.height
+        );
+
+        // Only overwrite existing pixels (i.e. the segmentation mask)
+        canvasCtx.globalCompositeOperation = "source-in";
+        canvasCtx.drawImage(
+            results.image,
+            0,
+            0,
+            canvasRef.current.width,
+            canvasRef.current.height
+        );
+
+        // Only overwrite missing pixels (i.e. anything outside of the segmentation mask)
+        canvasCtx.globalCompositeOperation = "destination-atop";
+        canvasCtx.fillStyle = "#00FF00";
+        canvasCtx.fillRect(
+            0,
+            0,
+            canvasRef.current.width,
+            canvasRef.current.height
+        );
+        canvasCtx.restore();
     };
 
     return (
